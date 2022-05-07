2f45cefc8f609fef102b01fa7a1f1262605fb390
CGfrac <CGfrac@protonmail.com>
2022-05-07 10:27:59 +0200

    Implement base functionality to calculator

--- script.js
+++ script.js
@@ -24,12 +24,73 @@ function operate(a, b, operator) {
         case '-':
             result = substract(a, b);
             break;
-        case '*':
+        case '\xD7':
             result = multiply(a, b);
             break;
-        case '/':
+        case '\xF7':
             result = divide(a, b);
             break;
     }
     return result;
 }
+
+function updateDisplay(value) {
+    display.textContent = value;
+}
+
+function updateValue(value, newValue) {
+    const updatedValue = (value === '0') ? newValue : value + newValue;
+    updateDisplay(updatedValue);
+    return updatedValue;
+}
+
+function giveResult() {
+    if (operator === '') return;
+
+    const result = operate(+a, +b, operator);
+    updateDisplay(result);
+    a = `${result}`; // We keep it on a to chain operations
+    b = '0';
+}
+
+function clear() {
+    a = '0';
+    b = '0';
+    operator = '';
+    firstOperand = true;
+    updateDisplay(a);
+}
+
+function processButton(event) {
+    const button = event.target;
+    const content = button.textContent;
+
+    if (content === '=') {
+        giveResult();
+    } else if (content === 'C') {
+        clear();
+    } else if (button.classList.contains('operand')) {
+        if (firstOperand) {
+            a = updateValue(a, content);
+        } else {
+            b = updateValue(b, content);
+        }
+    } else if (button.classList.contains('operator')) {
+        if (firstOperand) {
+            firstOperand = false;
+        } else {
+            giveResult();
+        }
+        operator = content;
+    }
+}
+
+const display = document.querySelector('#display p');
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(button => button.addEventListener('click', processButton));
+
+let a = '0';
+let b = '0';
+let operator = '';
+let firstOperand = true;
